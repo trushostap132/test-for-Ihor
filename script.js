6dd04c06ceeacb8f6f4366ada460b8c5c3ad31e0
Ostap Trush <43958957+pawas007@users.noreply.github.com>
2018-11-21 00:08:59 +0300

    + nav bar animations

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
-$(document).ready(function () {
-    $('#web-des').on('click', function () {
-        $('.ex-container li').hide();
-        $('.web-des-it').show('slow');
-    });
+$(document).ready(function() {
+  $("#web-des").on("click", function() {
+    $(".ex-container li").hide();
+    $(".web-des-it").show("slow");
+  });
 
-    $('#graphic').on('click', function () {
-        $('.ex-container li').hide();
-        $('.graphic').show('slow');
-    });
+  $("#graphic").on("click", function() {
+    $(".ex-container li").hide();
+    $(".graphic").show("slow");
+  });
+
+  $("#flat").on("click", function() {
+    $(".ex-container li").hide();
+    $(".flat").show("slow");
+  });
+
+  $("#all").on("click", function() {
+    $(".ex-container li").show("slow");
+  });
 
 
-    $('#flat').on('click', function () {
-        $('.ex-container li').hide();
-        $('.flat').show('slow');
-    });
-
-    $('#all').on('click', function () {
-        $('.ex-container li').show('slow');
-    });
 
 });
