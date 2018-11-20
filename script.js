9ace9f081c7d558d6e9ebc0d8440b72595e67824
Ostap Trush <43958957+pawas007@users.noreply.github.com>
2018-11-20 21:59:48 +0300

    commit 1

---
+++ script.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $('#web-des').on('click', function () {
+        $('.ex-container li').hide();
+        $('.web-des-it').show('slow');
+    });
+
+    $('#graphic').on('click', function () {
+        $('.ex-container li').hide();
+        $('.graphic').show('slow');
+    });
+
+
+    $('#flat').on('click', function () {
+        $('.ex-container li').hide();
+        $('.flat').show('slow');
+    });
+
+    $('#all').on('click', function () {
+        $('.ex-container li').show('slow');
+    });
+
+});
